21effbbb7690c4a0cf5ec7ea5bdda91814a509a5
danniiigt <danielgonzalezt13@gmail.com>
2023-12-07 23:30:45 +0100

    Added english language

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -75,6 +75,56 @@ export const ui = {
     About: "A bit about me",
     AboutSubtitle:
       "How I got into the world of development and my plans for the future. 👇",
+    Experience: "My experience in the tech world",
+    ExperienceSubtitle:
+      "The passage through the different projects and my role in each one 💻",
+    DownloadCV: "Download CV",
+    Contact: "Contact",
+    Stack: "Tech Stack",
+    StackSubtitle: "The technologies I use on a daily basis 🧩",
+    Languages: "Programming Languages",
+    Frameworks: "Frameworks and Libraries",
+    Software: "Software and Applications",
+    Cloud: "Cloud Services",
+    Projects: "My personal projects",
+    ProjectsSubtitle: "Some of the ones I've been able to finish... 🙄",
+    WebBuild: "Web built with",
+    And: "and",
+    Deployed: "Deployed in",
+    MeDescription:
+      "Passionate about technology and the world of development. I love learning and transforming ideas into realities.",
+    AboutParagraph1:
+      "At the age of 16, while attending high school, a spark of boredom ignited my curiosity and propelled me to venture into the fascinating world of web development. In search of something new, I decided to immerse myself in learning and development, marking the beginning of my journey in this exciting sector. What started as a way to break the monotony quickly turned into a passion for creating, learning, and growing in the realm of web development.",
+    AboutParagraph2:
+      "My ambition and curiosity have enabled me to learn the technologies I currently use, alongside pursuing a higher degree in web application development where I have also cultivated collaborative skills, among others. I have always been a restless individual who enjoys creating, learning, and self-improvement.",
+    AboutParagraph3:
+      "My vision extends beyond my own career; I am committed to contributing to the future of development, where collaboration, sustainability, and accessibility are fundamental pillars. I see collaboration as the driving force behind innovation, the ability to work hand in hand with colleagues and communities to create more impactful and robust solutions.",
+    AboutParagraph4:
+      "I am excited about the opportunities that the future of development presents, and I am eager to be a part of it.",
+    ExperienceDate1: "JUN 2023 - CURRENT",
+    ExperienceTitle1: "Fullstack Developer (React.js & Node.js) @ Papel A Web",
+    ExperienceBody1:
+      "In this role, I once again engage in autonomous web application development. My daily tasks involve analyzing the needs of the client/company and crafting a tailored technical solution, covering the entire process: designing the database, backend development, and frontend development. This experience has afforded me the opportunity to tackle ongoing challenges and continuously expand my knowledge.",
+    ExperienceDate2: "MAR 2023 - JUN 2023",
+    ExperienceTitle2: "Fullstack Developer (VUE & .NET) @ Comanet",
+    ExperienceBody2:
+      "At Comanet, I performed the role of fullstack developer. It was a new challenge, as I faced a technological stack that I was unfamiliar with, with technologies such as Vue and .NET. After just a few weeks, I was able to master both technologies and began working autonomously on the development of a webapp for racing events. This application encompassed various functionalities, such as registration management, payments, and participants, among others. My experience at Comanet not only allowed me to acquire new skills, but also to develop the ability to quickly adapt to changing technological environments.",
+    ExperienceDate3: "DEC 2022 - MAR 2023",
+    ExperienceTitle3: "Freelance Fullstack Developer",
+    ExperienceBody3:
+      "My first experience both as an employee and as a freelancer. I was developing technical solutions for different clients, from a web for order tracking, to a REST API for an investment application with integration of different payment methods. During this stage I was able to put my web development knowledge into practice, and learn to work with clients, analyzing their needs and offering tailored solutions.",
+    ProjectTitle1: "Tech Blog",
+    ProjectBody1:
+      "My tech blog, where I write about topics such as web development, programming, technology, etc. It is built with Next.js, TailwindCSS and Shadcn/ui. For content management I use the Sanity.io API, which allows me to create a headless CMS and write my articles from anywhere without having to touch any code.",
+    ProjectTitle2: "Airbnb Clone",
+    ProjectBody2:
+      "A detailed clone of the Airbnb website. Built with Next.js on version 13 (App dir) with dynamic and other static pages. Authentication is done through NextAuth.js connected to a MongoDB database.",
+    ProjectTitle3: "Spotify Clone",
+    ProjectBody3:
+      "A clone of the famous Spotify. Built on Next.js 12 (Pages dir) and TailwindCSS. Authentication, database and image/song storage is done through Supabase.",
+    ProjectTitle4: "Rentacar",
+    ProjectBody4:
+      "Car rental website. A static website, without much functionality, but with a very careful design. Built with Next.js and TailwindCSS.",
   },
 } as const;
 
