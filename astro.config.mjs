8a2fb2c0a86de61ce597a4017d273d470f34c5f2
danniiigt <danielgonzalezt13@gmail.com>
2024-04-17 11:45:50 +0200

    chore: añadida dependencia de astro vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ export default defineConfig({
   integrations: [tailwind()],
   output: "server",
   adapter: vercel({
-    webAnalytics: { enabled: true },
+    webAnalytics: {
+      enabled: true,
+    },
   }),
 });
